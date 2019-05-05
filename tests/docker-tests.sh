b4a0c8323a9a30493a20bb1eb841bf33e1870faa
Maurizio Brioschi <m.brioschi@vitafy.de>
2019-05-05 22:31:03 +0200

    add tests

---
+++ tests/docker-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mounting_volume()   {
+mkdir data;
+docker run -d --name=my-hexo-blog -p 4000:4000 -v $(pwd)/data/:/home/hexo/blog hexo-alpine:latest
+if [ -z "$(ls -A data)" ]; then
+   echo "DIRECTORY EMPTY";
+   ls -altr data;
+   exit 1;
+else
+   echo "NOT EMPTY";
+fi
+docker stop my-hexo-blog;
+}
+
+mounting_volume();
