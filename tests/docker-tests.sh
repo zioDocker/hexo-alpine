08d4b6bdf3d785a0cc75f6d971065e9ed8dd92b5
Maurizio Brioschi <m.brioschi@vitafy.de>
2019-05-06 09:00:48 +0200

    no mount test

--- tests/docker-tests.sh
+++ tests/docker-tests.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+docker_run_no_mount()    {
+    docker run -d --name=my-hexo-blog-no-mount -p 4000:4000 hexo-alpine:latest;
+    docker rm my-hexo-blog-no-mount -f;
+    echo "docker_run_no_mount OK";
+}
+
 mounting_volume()   {
 mkdir data;
-docker run -d --name=my-hexo-blog -p 4000:4000 -v $(pwd)/data/:/home/hexo/blog hexo-alpine:latest
+docker run -d --name=my-hexo-blog-mount -p 4000:4000 -v $(pwd)/data/:/home/hexo/blog hexo-alpine:latest
 if [ -z "$(ls -A data)" ]; then
    echo "DIRECTORY EMPTY";
    ls -altr data;
@@ -11,7 +17,9 @@ else
    echo "NOT EMPTY";
 fi
 rm -rf data;
-docker stop my-hexo-blog;
+docker rm my-hexo-blog-mount -f;
+echo "mounting_volume OK";
 }
 
+docker_run_no_mount
 mounting_volume
