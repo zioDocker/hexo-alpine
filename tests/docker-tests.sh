087cbd11423fb44957f8dd98ca0d7a776e184b79
Maurizio Brioschi <m.brioschi@vitafy.de>
2019-05-06 09:24:23 +0200

    replace echo with printf

--- tests/docker-tests.sh
+++ tests/docker-tests.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 docker_run_no_mount()    {
-    echo "\n docker_run_no_mount START";
+    printf "\n docker_run_no_mount START";
     docker run -d --name=my-hexo-blog-no-mount -p 4000:4000 hexo-alpine:latest;
     docker rm my-hexo-blog-no-mount -f;
-    echo "\n docker_run_no_mount OK";
+    printf "\n docker_run_no_mount OK";
 }
 
 mounting_volume()   {
-echo "\n mounting_volume START";
+printf "\n mounting_volume START";
 mkdir data;
 docker run -d --name=my-hexo-blog-mount -p 4000:4000 -v $(pwd)/data/:/home/hexo/blog hexo-alpine:latest
 if [ -z "$(ls -A data)" ]; then
-   echo "\n DIRECTORY EMPTY";
+   printf "\n DIRECTORY EMPTY";
    ls -altr data;
    exit 1;
 else
-   echo "\n DIRECTORY NOT EMPTY";
+   printf "\n DIRECTORY NOT EMPTY";
 fi
 rm -rf data;
 docker rm my-hexo-blog-mount -f;
-echo "\n mounting_volume OK";
+printf "\n mounting_volume OK";
 }
 
 docker_run_no_mount
